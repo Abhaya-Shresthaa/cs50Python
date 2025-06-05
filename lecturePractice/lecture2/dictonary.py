a2b93478eda7f71dbd86a8de56a8e0ea67faaaea
Abhaya-Shresthaa <gopalkumarshrestha123@gmail.com>
2025-06-05 22:35:03 +0545

    dictionary file added

---
+++ lecturePractice/lecture2/dictonary.py
@@ -0,0 +1,18 @@
+students = {
+    "Abhaya" : "Shrestha",
+    "Chandan" : "Shah",
+    "Kushal" : "Regmi",
+    "Asmit" : "Khana;",
+    "Darpan" : "Giri"
+}
+
+print(students["Abhaya"])
+print(students["Asmit"])
+print(students["Chandan"])
+print(students["Darpan"])
+print(students["Kushal"])
+
+
+
+
+
