28b4064056a6ff7c7dec1fc2094dbab8a20c6567
Abhaya-Shresthaa <gopalkumarshrestha123@gmail.com>
2025-06-08 23:56:03 +0545

    problem set 3

---
+++ problemSets/ProblemSet3/fuel/fuel.py
@@ -0,0 +1,24 @@
+while True: 
+    try:
+        userInput= input("Fraction: ")
+        x, y = userInput.split('/')
+        x = int(x)
+        y = int(y)
+        percentage = int((x/y)*100)
+        if x > y:
+            continue
+    except ValueError:
+        pass
+    except ZeroDivisionError:
+        pass
+        
+    else:
+        break
+    
+if percentage <= 1:
+    print("E")
+elif percentage >= 99:
+    print("F")
+else:
+    print(f"{percentage}%")
+
