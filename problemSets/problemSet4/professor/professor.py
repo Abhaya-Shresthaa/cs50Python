6952a86219f45ae3ab734d57c28303807cc07d78
Abhaya-Shresthaa <gopalkumarshrestha123@gmail.com>
2025-06-11 20:54:47 +0545

    changes in professor file

--- problemSets/problemSet4/professor/professor.py
+++ problemSets/problemSet4/professor/professor.py
@@ -1,28 +1,5 @@
 from random import randint
 
-score = 0
-while True:
-    level = int(input("Level: "))
-    if 0 < level < 4:
-        break
-  
-for _ in range(1, 10):
-    x = randint((10**(level-1)),((10**level)-1))
-    y = randint((10**(level-1)),((10**level)-1))
-    for i in range(3):
-        userAns = int(input(f"{x} + {y} = "))
-        if(userAns == (x+y)):
-            score = score +1
-            break
-        elif i == 2:
-            print("EEE")
-            print(f"{x} + {y} = {x+y}")
-        else:
-            print("EEE")
-
-import random
-
-
 def main():
     score = 0
     level = get_level()
@@ -39,8 +16,6 @@ def main():
                 print(f"{x} + {y} = {x+y}")
             else:
                 print("EEE")
-
-
 
 def get_level():
     while True:
